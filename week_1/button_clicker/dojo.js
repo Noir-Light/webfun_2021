e16e36e4411f497970d7192d2d86d36d772a4faf
Noir-Light <lanfearjohn63@gmail.com>
2021-07-23 13:35:34 -0700

    w1d4 finished

--- week_1/button_clicker/dojo.js
+++ week_1/button_clicker/dojo.js
@@ -30,4 +30,3 @@ function hide(element) {
     element.remove()
 }
 
-
--- week_1/likes/dojo.js
+++ week_1/likes/dojo.js
@@ -1,6 +1,29 @@
-var click = 0
 
-function countLikes(element) {
+var click = 0;
+function countLikes1(element) {
     click++
     element.innerText = click + "Likes"
 }
+
+
+
+var click1 = 0;
+function countLikes2(element) {
+    click1++
+    element.innerText = click1 + "Likes"
+}
+
+
+
+var click2 = 0;
+function countLikes3(element) {
+    click2++
+    element.innerText = click2 + "Likes"
+}
+
+
+// var click = 0;
+// function countLikes(element){
+//     click += 1;
+//     element.innerText = click + "Likes";
+// }
